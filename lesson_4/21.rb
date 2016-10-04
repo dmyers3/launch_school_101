22ab2eaf520f9b8ce301afb4bdfdb5589ce7b75e
Daniel Myers <daniel.p.myers@gmail.com>
2016-10-04 23:08:45 +0000

    add suit icons

--- lesson_4/21.rb
+++ lesson_4/21.rb
@@ -1,25 +1,34 @@
 require 'pry'
+require 'to_words'
 GAME_TGT = 21
 DLR_STAY = 17
-SUITS = ['H', 'D', 'S', 'C'].freeze
+SUITS_CHAR = ["\u2665", "\u2666", "\u2660", "\u2663"].freeze
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9',
           'T', 'J', 'Q', 'K', 'A'].freeze
+TOT_TO_WIN = 5
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def initialize_deck
-  SUITS.product(VALUES)
+  SUITS_CHAR.product(VALUES)
 end
 
+def display_score(score)
+  prompt("Current score is You: #{score[:player_count]}")
+  prompt("              Dealer: #{score[:computer_count]}")
+end
+
+# displays cards in graphic format
+# hides dealers first card as well while still preserving its value
 # rubocop:disable Metrics/AbcSize
 def card_display(cards, num_cards, hidden = false)
   dealer_first_card = cards[0] if hidden == true
   cards[0] = [' ', ' '] if hidden == true
   num_cards.times { print "  ______ " }
   puts ""
-  num_cards.times { |num| print " |#{cards[num][0]}     |" }
+  num_cards.times { |num| print " |#{cards[num][0].encode('utf-8')}     |" }
   puts ""
   num_cards.times { print " |      |" }
   puts ""
@@ -27,7 +36,7 @@ def card_display(cards, num_cards, hidden = false)
   puts ""
   num_cards.times { print " |      |" }
   puts ""
-  num_cards.times { |num| print " |_____#{cards[num][0]}|" }
+  num_cards.times { |num| print " |_____#{cards[num][0].encode('utf-8')}|" }
   puts ""
   cards[0] = dealer_first_card if hidden == true
 end
@@ -65,11 +74,11 @@ def ace_check(val, aces)
   val
 end
 
-def display_hands(d_cards, p_cards, values, hidden = false)
+def display_hands(d_cards, p_cards, totals, hidden = false)
   system "clear"
-  prompt("Dealer is showing: #{values[:dealer]}")
+  prompt("Dealer is showing: #{totals[:dealer]}")
   card_display(d_cards, d_cards.length, hidden)
-  prompt("Player is showing: #{values[:player]}")
+  prompt("You are showing: #{totals[:player]}")
   card_display(p_cards, p_cards.length)
 end
 
@@ -85,43 +94,42 @@ def player_choice
   answer
 end
 
-def player_loop(d_cards, p_cards, values, deck)
-  while values[:player] <= GAME_TGT
+def player_loop(d_cards, p_cards, totals, deck)
+  while totals[:player] <= GAME_TGT
     case player_choice
     when 'h'
       p_cards = deal(deck, p_cards)
-      values[:player] = value_calc(p_cards)
-      display_hands(d_cards, p_cards, values, true)
+      totals[:player] = value_calc(p_cards)
+      display_hands(d_cards, p_cards, totals, true)
     when 's'
       break
     end
   end
 end
 
-def dealer_loop(d_cards, p_cards, values, deck)
+def dealer_loop(d_cards, p_cards, totals, deck)
   prompt("Dealer is flipping over his hidden card...")
   sleep 1
-  values[:dealer] = value_calc(d_cards)
-  display_hands(d_cards, p_cards, values)
+  totals[:dealer] = value_calc(d_cards)
+  display_hands(d_cards, p_cards, totals)
   sleep 1.5
-  while values[:dealer] < DLR_STAY
-    prompt("Dealer hits under #{DLR_STAY}")
+  while totals[:dealer] < DLR_STAY
     d_cards = deal(deck, d_cards)
-    values[:dealer] = value_calc(d_cards)
-    display_hands(d_cards, p_cards, values)
-    sleep 2
+    totals[:dealer] = value_calc(d_cards)
+    display_hands(d_cards, p_cards, totals)
+    sleep 1.5
   end
 end
 
-def check_for_bust(values)
-  if values[:player] > GAME_TGT
+def bust?(totals)
+  if totals[:player] > GAME_TGT
     display_result(:bust)
     return true
-  elsif values[:dealer] > GAME_TGT
+  elsif totals[:dealer] > GAME_TGT
     display_result(:dealer_bust)
     return true
   end
-  nil
+  false
 end
 
 def display_result(result)
@@ -139,13 +147,23 @@ def display_result(result)
   end
 end
 
-def compare_values(values)
-  if values[:player] == values[:dealer]
+def determine_winner(totals, score)
+  if totals[:player] == totals[:dealer]
     display_result(:tie)
-  elsif values[:player] > values[:dealer]
+  elsif totals[:player] > totals[:dealer]
     display_result(:win)
+    score[:player_count] += 1
   else
     display_result(:lose)
+    score[:computer_count] += 1
+  end
+end
+
+def end_of_game_display(score)
+  if score[:player_count] == TOT_TO_WIN
+    prompt("You won the whole game!")
+  elsif score[:computer_count] == TOT_TO_WIN
+    prompt("Dealer wins the whole game")
   end
 end
 
@@ -160,32 +178,42 @@ def play_again?
   answer == 'y'
 end
 
-prompt("Welcome to Twenty-One! Try to get close to 21 without going over")
-prompt("Dealer will stay at 17")
-sleep 5
+prompt("Welcome to #{GAME_TGT.to_words.upcase}!")
+prompt("Try to get as close as possible to #{GAME_TGT} without going over")
+prompt("Dealer will stay at #{DLR_STAY}")
+prompt("First to #{TOT_TO_WIN} wins!")
+sleep 4
 
 loop do
-  deck = initialize_deck
-  player_cards = []
-  dealer_cards = []
+  score = { player_count: 0, computer_count: 0 }
 
-  initial_hands(deck, player_cards, dealer_cards)
-  values = { player: value_calc(player_cards),
-             dealer: value_calc(dealer_cards.values_at(1..-1)) }
-  display_hands(dealer_cards, player_cards, values, true)
-
-  player_loop(dealer_cards, player_cards, values, deck)
-  if check_for_bust(values)
-    play_again? ? next : break
+  while score[:player_count] < TOT_TO_WIN && score[:computer_count] < TOT_TO_WIN
+    display_score(score)
+    sleep 3
+    deck = initialize_deck
+    player_cards = []
+    dealer_cards = []
+  
+    initial_hands(deck, player_cards, dealer_cards)
+    totals = { player: value_calc(player_cards),
+               dealer: value_calc(dealer_cards.values_at(1..-1)) }
+    display_hands(dealer_cards, player_cards, totals, true)
+  
+    player_loop(dealer_cards, player_cards, totals, deck)
+    if bust?(totals)
+      score[:computer_count] += 1 
+      next
+    end
+  
+    dealer_loop(dealer_cards, player_cards, totals, deck)
+    if bust?(totals)
+      score[:player_count] += 1
+      next
+    else
+      determine_winner(totals, score)
+    end
   end
-
-  dealer_loop(dealer_cards, player_cards, values, deck)
-  if check_for_bust(values)
-    play_again? ? next : break
-  else
-    compare_values(values)
-  end
-
+  end_of_game_display(score)
   break unless play_again?
 end
 
